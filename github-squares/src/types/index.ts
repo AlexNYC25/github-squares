8d8906b3f6bf79b44c6e5deb52d3f9fca303c94d
Alexis Montes <montes.alexis25@gmail.com>
2026-01-19 16:23:10 -0500

    Added github settings type to the types

--- github-squares/src/types/index.ts
+++ github-squares/src/types/index.ts
@@ -1,3 +1,4 @@
 
 export * from "./github.type";
 export * from "./response.type";
+export * from "./settings.type";
---
+++ github-squares/src/types/settings.type.ts
@@ -0,0 +1,5 @@
+
+export type GithubSquaresSettings = {
+	apikey: string;
+	daysBack: number;
+};
